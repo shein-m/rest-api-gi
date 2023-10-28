6f627a1fc8c3fcba94f5b5b8ae4461ea6e5069d0
Nikolay Shein <qseboy1998@gmail.com>
2023-10-28 17:21:24 +0300

    change fileApi to mongoose, and middleware for id, handleMongooseError, add schema and model to contact

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { DB_CONNECT, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_CONNECT)
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
